386c936fc1347207a601b2376dbea6ab21229ede
Ali Hasan <capt.3lii@gmail.com>
2024-12-21 16:16:47 +0300

    Update project configuration and dependencies; refine App component structure

--- App.tsx
+++ App.tsx
@@ -1,44 +1,27 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-
-// import {Authenticator, useAuthenticator} from '@aws-amplify/ui-react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
-import {AmplifyConfiguration} from './amplify/config';
+import {AmplifyConfiguration} from './amplify/config'; // Ensure this file exists and is correct
 import AppStack from './src/stack/AppStack';
 
 AmplifyConfiguration();
 
-// const SignOutButton = () => {
-//   const {signOut} = useAuthenticator();
-
-//   return (
-//     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-//       <Button title="Sign Out" onPress={signOut} />
-//     </View>
-//   );
-// };
 const App = () => {
   return (
     <NavigationContainer>
       <SafeAreaView style={styles.container}>
-        {/* Comment the Authenitcator till adding the backend side + update .env */}
-        {/* <Authenticator.Provider>
-          <Authenticator> */}
-            <AppStack />
-            {/* <SignOutButton />
-          </Authenticator> */}
-        {/* </Authenticator.Provider> */}
+        <AppStack />
       </SafeAreaView>
     </NavigationContainer>
   );
 };
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
   },
-};
+});
 
 export default App;
